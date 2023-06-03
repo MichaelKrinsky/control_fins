04bb3e04bf379a189b06fef564b628f9e2333d45
MichaelKrinsky <34221696+MichaelKrinsky@users.noreply.github.com>
2023-06-03 12:59:34 -0700

    Updated To include PID controll along with kinematic and newton's

--- Controller/src/finpair.cpp
+++ Controller/src/finpair.cpp
@@ -13,11 +13,17 @@ void FinPair::calibrate(){
     offset = 0;
 }
 
+float getAirDensity(float altitude){ // In meters
+    float e = 2.718;
+    return 1.28 * pow(e,-1.2 * (pow(10,-4)* altitude));
+}
 float FinPair::determineFinAngle(float altitude, float speed, float rollError, float rollVelocity, float directionError, float directionVelocity) { 
     // Torque = inertia * acceleration
     // Xf = X0 + VoT + 1/2AT**2
     // Lift = Clift * density * V**2 * wingArea / 2
-    float airDensity = altitude * 0.5; // Some calculation for air density
+    float airDensity = getAirDensity(altitude); // Some calculation for air density
+    //Density pressure altitude table for air density
+    //Look into energetics Work-energy theorem instead of solving for the acceleration?
     float desiredAcceleration = -2 * (directionError + directionVelocity * 2) / 4; // Solve given the directionError and directionVelocity A = -2(X0+VT)/ (T**2)
     float requiredLift = desiredAcceleration * momentOfInertia / cgToFinY; // "lift" is required force in the proper direction to achieve acceleration
     float desiredCoefficientOfLift = requiredLift / ( airDensity * pow(speed, 2) * finArea);
--- Controller/src/main.cpp
+++ Controller/src/main.cpp
@@ -4,9 +4,6 @@
 #include "gyro.h"
 #include "gps.h"
 #include "finpair.h"
-Gyro gyroSensor;
-IMU imuSensor;
-std::vector<Fin> fins(3);
 const float kP = 0.1; // Proportional gain
 const float kI = 0.01; // Integral gain
 const float kD = 0.5; // Derivative gain
@@ -15,60 +12,74 @@ const int MAX_ANGLE = 90;
 float angle = 0;
 float altitude = 0;
 float linear_speed = 0;
-float pitch = 0;
-float roll = 0;
-float yaw = 0;
 // float pitch_velocity = 0;
 // float roll_velocity = 0;
 // float yaw_velocity = 0;
 float pitchFinAngle = 0;
 float yawFinAngle = 0;
 
-float error = 0; // Current error
-float prev_error = 0; // Error from previous iteration
-float integral = 0; // Accumulated error
-float derivative = 0; // Error rate of change
-float output = 0; // Output value
+float pitchError = 0; // Current error
+float prevPitchError = 0; // Error from previous iteration
+float pitchIntegral = 0; // Accumulated error
+float pitchDerivative = 0; // Error rate of change
+float pitchOutput = 0; // Output value
+const float pitchkP = 0.1; // Proportional gain
+const float pitchkI = 0.01; // Integral gain
+const float pitchkD = 0.5; // Derivative gain
+
+
+float yawError = 0; // Current error
+float prevYawError = 0; // Error from previous iteration
+float yawIntegral = 0; // Accumulated error
+float yawDerivative = 0; // Error rate of change
+float yawOutput = 0; // Output value
+const float yawkP = 0.1; // Proportional gain
+const float yawkI = 0.01; // Integral gain
+const float yawkD = 0.5; // Derivative gain
+
+float rollError = 0; // Current error
+float prevRollError = 0; // Error from previous iteration
+float rollIntegral = 0; // Accumulated error
+float rollDerivative = 0; // Error rate of change
+float rollOutput = 0; // Output value
+const float rollkP = 0.1; // Proportional gain
+const float rollkI = 0.01; // Integral gain
+const float rollkD = 0.5; // Derivative gain
+
+FinPair pitchFins;
+FinPair yawFins;
+IMU imuSensor;
+GPS gpsSensor;
+Gyro gyroSensor;
 
 void setup() {
-    IMU imuSensor = IMU{};
-    Gyro gyroSensor = Gyro{};
-    GPS gpsSensor = GPS{};
-    FinPair pitchFins;
-    FinPair yawFins;
+    imuSensor = IMU{};
+    gyroSensor = Gyro{};
+    gpsSensor = GPS{};
 }
 
 
 void loop() {
-    angular_velocity = imuSensor.getRotationalSpeed();
-    altitude = gpsSensor.getAltitude();
-    linear_speed = imuSensor.getSpeed('y');
-    pitch = gyroSensor.getAngle('x');
-    yaw = gyroSensor.getAngle('y')
-    roll = gyroSensor.getAngle('z');
+    float angular_velocity = imuSensor.getRotationalSpeed('x');
+    float altitude = gpsSensor.getAltitude();
+    float linear_speed = imuSensor.getSpeed('y');
+    pitchError = gyroSensor.getAngle('x');
+    yawError = gyroSensor.getAngle('y');
+    rollError = gyroSensor.getAngle('z');
 
-    pitchFinAngle = pitchFins.determineFinAngle(altitude, linear_speed, roll, 0, pitch, 0);
-    yawFinAngle = yawFins.determineFinAngle(altitude, linear_speed, roll, 0, yaw, 0);
-    
-    // pitch_velocity = imuSensor.getRotationalSpeed("z");
-    
-
-    // error = current_angle;
   
-    // integral += error;
-    // derivative = error - prev_error;
-    // output = kP * error + kI * integral + kD * derivative;
+    rollIntegral += rollError;
+    rollDerivative = rollError - prevRollError;
+    rollOutput = rollkP * rollError + rollkI * rollIntegral + rollkD * rollDerivative;
 
-    // if (output > MAX_ANGLE) {
-    // output = MAX_ANGLE;
-    // } else if (output < -MAX_ANGLE) {
-    // output = -MAX_ANGLE;
-    // }
+    pitchIntegral += pitchError;
+    pitchDerivative = pitchError - prevPitchError;
+    pitchOutput = pitchkP * pitchError + pitchkI * pitchIntegral + pitchkD * pitchDerivative;
+
+    yawIntegral += yawError;
+    yawDerivative = yawError - prevYawError;
+    yawOutput = yawkP * yawError + yawkI * yawIntegral + yawkD * yawDerivative;
     
-    // for( auto i = 0; i != fins.size(); i++){
-    //     fins[i].setAngle(output);
-    // }
-    
-    // Update previous error
-    // prev_error = error;
+    pitchFinAngle = pitchFins.determineFinAngle(altitude, linear_speed, rollOutput, 0, pitchOutput, 0);
+    yawFinAngle = yawFins.determineFinAngle(altitude, linear_speed, rollOutput, 0, yawOutput, 0);
 }
